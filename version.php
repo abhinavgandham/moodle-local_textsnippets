a76a4fd34d6b758c078e726d278d984595813fa6
Abhinav Gandham <abhinavgandham@catalyst-au.net>
2026-06-01 21:20:39 +1000

    Init: Initial plugin skeleton.

---
+++ version.php
@@ -0,0 +1,9 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_feedbackbank';
+$plugin->version   = 2026052500;
+$plugin->requires  = 2025041400;
+$plugin->maturity  = MATURITY_ALPHA;
+$plugin->release   = 'v0.1.0';
+$plugin->supported = [405, 503];
